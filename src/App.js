0866efd2f4e70f6a35be1bf37a4dbaca1978ab20
Jonah Lewis <jonahslewis96@gmail.com>
2024-01-22 15:01:32 -0800

    added to about.js file

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from "react";
 import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
 import Navbar from "./components/Navbar";
+import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Testimonials from "./components/Skills";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
-    <main>
+    <main className="text-gray-400 bg-gray-900 body-font">
       <Navbar />
       <About />
       <Projects />
